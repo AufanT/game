67d14536df8af8272ce1d518e17e97721729382d
AufanT <aufantaufiq08905@gmail.com>
2023-11-29 20:33:08 +0700

    game angka boom

---
+++ script.js
@@ -0,0 +1,38 @@
+var tanya = true;
+while (tanya) {
+  // menangkap pilihan player
+  var p = prompt("masukkan : Gajah, Semut, atau Orang");
+
+  // menangkap pilihan komputer
+  // membangkitkan bilangan random
+  var comp = Math.random();
+
+  if (comp < 0.34) {
+    comp = "Gajah";
+  } else if (comp >= 0.34 && comp <= 0.67) {
+    comp = "Semut";
+  } else {
+    comp = "Orang";
+  }
+
+  var hasil = "";
+  // menentukan rules
+  if (p == comp) {
+    hasil = "Seri";
+  } else if (p == "Gajah") {
+    hasil = (comp == "Semut") ? "Kalah" : "Menang";
+  } else if (p == "Semut") {
+    hasil = (comp == "Orang") ? "Kalah" : "Menang";
+  } else if (p == "Orang") {
+    hasil = (comp == "Gajah") ? "Kalah" : "Menang";
+  } else {
+    hasil = "Anda memasukkan pilihan yang tidak sesuai";
+  }
+
+  // hasil
+  alert(`Pilihanmu: ${p} \nPilihan AI: ${comp} \nPertandingan: ${hasil}`);
+
+  tanya = confirm("Try again?");
+}
+
+alert("Terimakasih sudah bermain");
---
+++ tebakAngka.js
@@ -0,0 +1,46 @@
+// menangkap pilihan komputer
+// membangkitkan bilangan random
+var maxNumber = 10;
+var AngkaBoom = Math.floor(Math.random() * maxNumber) + 1;
+var canceled = false; // Mengatur variabel untuk mengecek apakah permainan dibatalkan
+console.log(AngkaBoom)
+
+// jumlah tebakan
+
+for ( var i = 3; i > 0; i-- ) {
+  // menangkap pilihan player
+  var player = prompt("Angka Boom 1-10 \nHanya terdapat 3 kali percobaan");
+
+  if (player === null) {
+    canceled = true; // Mengubah status menjadi permainan dibatalkan
+    break;
+  }
+  
+  var result = "";
+  
+  // rules
+  if (player == AngkaBoom) {
+    alert('BOOM!');
+    alert(`Selamat, Anda Menang!`);
+    break;
+  } else if (player < 1 || player > 10 || isNaN(player)) {
+    result = "Mohon masukkan Angka 1-10";
+  } else if (player > AngkaBoom) {
+    result = "Tebakan Anda Terlalu Tinggi";
+  } else if (player < AngkaBoom) {
+    result = "Tebakan Anda Terlalu Rendah";
+  }
+
+  //hasil
+  alert(result);
+
+  // membuat ucapan ketika player langsung menjawab dengan benar
+  if ( i > 1) {
+    alert(`Sisa percobaan anda tinggal ${i - 1}`)
+  } else {
+    alert(`Angka BOOM-nya adalah ${AngkaBoom}`)
+  }
+}
+
+alert("Terimakasih sudah bermain!");
+
